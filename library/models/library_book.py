8d4faba72b5cae7a299a7e66b447b459eeb0a866
Marcos Mela <marcos.mela@qubiq.es>
2023-11-21 18:19:16 +0100

    [ADD] Translation to spanish

--- library/models/library_book.py
+++ library/models/library_book.py
@@ -7,23 +7,23 @@ class LibraryBooks(models.Model):
     
     
     #Nombre Libro --> Caracteres
-    name = fields.Char(string='Name', required=True)
+    name = fields.Char(string='Name', required=True ,translate = True)
     
     #Precio --> Float
     price = fields.Float(string='Price')
     
     #Edicion --> Int
-    edition = fields.Integer(string='Edition')
+    edition = fields.Integer(string='Edition' ,translate = True)
     
     #Impreso o digital --> Selección
-    book_type = fields.Selection(string='Book type', selection=[('printed', 'Printed'), ('digital', 'Digital'),])
+    book_type = fields.Selection(string='Book type', selection=[('printed', 'Printed'), ('digital', 'Digital'),], translate = True)
     
     #Enlace web de compra --> Caracteres
-    web = fields.Char(string='Purchase Link')
+    web = fields.Char(string='Purchase Link',translate = True)
     
     #Se ha comprado --> Bool
-    is_purchased = fields.Boolean(string='Is purchased?')
+    is_purchased = fields.Boolean(string='Is purchased?',translate = True)
     
     #Fecha de compra --> DateTime
-    date = fields.Datetime(string='Date Purchase')
+    date = fields.Datetime(string='Date Purchase',translate = True)
     
