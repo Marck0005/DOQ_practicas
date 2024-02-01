683ae7708c8386d0801ff4c01628e7f0c8997d85
Marcos Mela <marcos.mela@qubiq.es>
2024-02-01 15:04:42 +0100

    [ADD] Graphs views

--- library/models/library_book_rent.py
+++ library/models/library_book_rent.py
@@ -13,11 +13,14 @@ class LibraryBookRent(models.Model):
     #Many2one porque muchos alquileres pueden tener el mismo libro
     book_id = fields.Many2one('library.book', string='Book')
     
+            
     rent_date = fields.Datetime(string='Rent date')
     devolution_date = fields.Datetime(string='Devolution date')
     real_devolution_date = fields.Datetime(string='Real devolution date')
     
     state = fields.Selection(string='State', selection = [('pending', 'Pending'), ('returned', 'Returned')])
+    
+    
     
     
     # x x x x x H x D 
